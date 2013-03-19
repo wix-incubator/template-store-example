4c97619be5c3a055897a2bfff79dae9192c70419
Evgeniy Kelyarsky <evgenek@wix.com>
2013-03-19 12:12:03 +0200

    typos fixed

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,5 +1,5 @@
 require 'active_merchant/billing/integrations/action_view_helper'
 ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)
-if Settings.paypal.sendbox
+if Settings.paypal.sandbox
   ActiveMerchant::Billing::Base.mode = :test
 end
