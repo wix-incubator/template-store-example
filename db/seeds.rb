de6a240f0dea58df530efee55f99b869327a6cc9
Evgeniy Kelyarsky <evgenek@wix.com>
2013-03-22 13:52:55 +0200

    new meta_site_ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
     name: 'Financial Consultants',
     site_url: 'http://templatesdream.wix.com/consulting-and-co',
     description: 'A sleek and sharp HTML template that shows just how professional your firm is. Easy to edit and update. Just add your own text and images and get your business online today!',
-    wix_meta_site_id: '12d31464-b76a-2235-350d-17dc301799ae',
+    wix_meta_site_id: '12ed689f-70e8-e44f-3482-dbf2fcd41b49',
     image_url: 'Financial Consultants.png',
     price: 15
   },
@@ -18,7 +18,7 @@
     name: 'Professional Photographer',
     site_url: 'http://templatesdream.wix.com/john-photographer',
     description: 'Get your photography business online today! This HTML template is super easy to edit, just add your own images and text and show the world your talent.',
-    wix_meta_site_id: '12d5c86a-52bf-56fe-1dfb-3cb9e533b7dc',
+    wix_meta_site_id: '12ed68dc-09e2-6e21-0cdd-7ee2323cf996',
     image_url: 'Professional Photographer.png',
     price: 12
   },
@@ -26,7 +26,7 @@
     name: 'Luxury Yachts',
     site_url: 'http://templatesdream.wix.com/luxury-yacht-free',
     description: 'Improving your online presence is a breeze with this beautiful HTML template. Easy to customize and update whenever you need.',
-    wix_meta_site_id: '12d62180-960a-bfe2-fd8a-8a92e0284214',
+    wix_meta_site_id: '12ed68de-b456-6fa8-2a07-01330e47df9b',
     image_url: 'Luxury Yachts.png',
     price: 12
   },
@@ -34,7 +34,7 @@
     name: 'Fashion & Style',
     site_url: 'http://templatesdream.wix.com/fashion-boutique',
     description: 'Get in style and start selling your products with this simple, chic HTML Ecommerce template.',
-    wix_meta_site_id: '12d268e1-731c-b2f9-736c-d6a0a98ed8e2',
+    wix_meta_site_id: '12ed68a7-a0c0-76fb-5382-7c5648f90a5e',
     image_url: 'Fashion & Style.png',
     note: "To activate the shopping cart for this template, you'll need to purchase the eCommerce premium package in Wix. For prices and more info <a href='http://www.wix.com/upgrade/website'>click here</a>",
     price: 20
@@ -43,7 +43,7 @@
     name: 'Car Dealer',
     site_url: 'http://templatesdream.wix.com/automike',
     description: 'Showcase your business online with this stylish HTML template. An excellent site for displaying your inventory and it\'s so easy to edit!',
-    wix_meta_site_id: '12d26066-12ec-3470-cc3d-5d4fc8e807fa',
+    wix_meta_site_id: '12ed6890-bee9-8dff-2606-782b35b200c1',
     image_url: 'Car Dealer.png',
     price: 12
   },
@@ -51,7 +51,7 @@
     name: 'The Author',
     site_url: 'http://templatesdream.wix.com/karen-parker-author',
     description: 'A thrilling HTML template that lets you show off your talents to the world. Just add your own text and images and get your portfolio online.',
-    wix_meta_site_id: '12d177f7-95b3-f71f-9053-8117886e3810',
+    wix_meta_site_id: '12ed68d4-eacb-dccd-d97f-321fe0444e00',
     image_url: 'The Author.png',
     price: 12
   },
@@ -59,7 +59,7 @@
     name: 'Fresh Meals',
     site_url: 'http://templatesdream.wix.com/cook-to-go',
     description: 'A sumptuous and delectable site that will leave your visitors drooling. This HTML template is great for displaying your images and includes plenty of room for text.',
-    wix_meta_site_id: '12d176da-87b3-d43a-9053-8117886e3810',
+    wix_meta_site_id: '12ed68f0-6956-a11b-5382-7c5648f90a5e',
     image_url: 'Fresh Meals.png',
     price: 12
   },
@@ -67,7 +67,7 @@
     name: 'Law Firm',
     site_url: 'http://templatesdream.wix.com/lawyers-and-co',
     description: 'Let your legal practice grow with this strong HTML template. Just add your own text and images and start building your online presence today.',
-    wix_meta_site_id: '12d12392-4237-41c3-82df-ca18e9bf9307',
+    wix_meta_site_id: '12ed68af-06fc-e917-5382-7c5648f90a5e',
     image_url: 'Law Firm.png',
     price: 15
   },
@@ -75,7 +75,7 @@
     name: 'Certified Accountant',
     site_url: 'http://templatesdream.wix.com/accountant-site',
     description: 'Create an HTML website that is high on content but still beautifully designed. Just add your own text and images and get your business online today!',
-    wix_meta_site_id: '12d13b81-cc4a-3c24-eeb2-03c9e5cf28a9',
+    wix_meta_site_id: '12ed68cb-4877-513c-2a07-01330e47df9b',
     image_url: 'Certified Accountant.png',
     price: 15
   },
@@ -83,7 +83,7 @@
     name: 'Personal Trainers',
     site_url: 'http://templatesdream.wix.com/the-shapers',
     description: 'Get the website your business deserves with this powerful HTML template and inspire your visitors. It\'s easy to edit, meaning your business can be online today!',
-    wix_meta_site_id: '12d13ed0-e278-fa81-43d3-11054598b0f0',
+    wix_meta_site_id: '12ed68f6-061a-9cd7-2606-782b35b200c1',
     image_url: 'Personal Trainers.png',
     price: 12
   },
@@ -91,7 +91,7 @@
     name: 'Cabin in the Woods',
     site_url: 'http://templatesdream.wix.com/cabins-tree',
     description: 'Start your online presence with this welcoming HTML template. It\'s easy to edit and personalize, simply add your own text and images and you\'re done.',
-    wix_meta_site_id: '12d18043-33e9-69af-cdc7-7a116ea29d08',
+    wix_meta_site_id: '12ed68fe-be9b-5322-2606-782b35b200c1',
     image_url: 'Cabin in the Woods.png',
     price: 12
   },
@@ -99,7 +99,7 @@
     name: 'Singer Songwriter',
     site_url: 'http://templatesdream.wix.com/musician',
     description: 'Get your music online with this striking HTML template. Simply upload your tracks, images and text and let your visitors hear how amazing you are.',
-    wix_meta_site_id: '12d3a672-34b6-1dd0-4018-d33586f74c6b',
+    wix_meta_site_id: '12ed68eb-a3f7-3d5f-3482-dbf2fcd41b49',
     image_url: 'Singer Songwriter.png',
     price: 12
   },
